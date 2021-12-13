efd198c3d01169d4875cc7069a94294b6903f6e1
rnnnmv4 <renannmv_4cavaleiros@hotmail.com>
2021-12-13 10:43:07 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/ocean_android_aplicacao_13_12_2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ocean_android_aplicacao_13_12_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
